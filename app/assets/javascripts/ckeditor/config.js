99a592827828277e7f9b9debd1b2cbe6879446c2
Juin Chiu <davidjuin0519@gmail.com>
2016-05-30 14:04:20 +0800

    remove all assets

--- app/assets/javascripts/ckeditor/config.js
+++
@@ -1,8 +0,0 @@
-CKEDITOR.editorConfig = function( config )
-{
-  // config.extraPlugins = 'markdown';
-  config.enterMode = CKEDITOR.ENTER_BR;
-  config.shiftEnterMode = CKEDITOR.ENTER_BR;
-  config.allowedContent = true;
-};
-
